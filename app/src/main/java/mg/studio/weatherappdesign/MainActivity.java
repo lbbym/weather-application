d4d11c069fd519e5df2d1636d631a4119ff94fe9
lbbym <liubbpro@gmail.com>
2019-03-04 16:40:02 +0800

    read json

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,7 +20,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,14 +32,41 @@ public class MainActivity extends AppCompatActivity {
     public void btnClick(View view) {
         new DownloadUpdate().execute();
     }
+    //
+    public class main {
+        public float temp;
+    }
+    public class weather {
+        public String description;
+    }
 
+    public class list {
+        //public int dt;
+        public String dt_txt;
+        public main main;
+        public weather[] weather;
+    }
+    public class  city {
+        public int id;
+        public String name;
+    }
 
+    public class WeatherRec {
+        public String cod;
+        //public int message;
+        //public int cnt;
+        public list[] list;
+        public city city;
+    }
+
+//
     private class DownloadUpdate extends AsyncTask<String, Void, String> {
 
 
         @Override
         protected String doInBackground(String... strings) {
-            String stringUrl = "https://mpianatra.com/Courses/info.txt";
+            String stringUrl = "https://mpianatra.com/Courses/forecast.json";
+            //https://www.jianshu.com/p/886f7b7fca7d
             HttpURLConnection urlConnection = null;
             BufferedReader reader;
 
@@ -56,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
                     // Nothing to do.
                     return null;
                 }
+
                 reader = new BufferedReader(new InputStreamReader(inputStream));
+                //Log.d("string：",inputStream.toString());
 
                 String line;
                 while ((line = reader.readLine()) != null) {
@@ -70,7 +102,13 @@ public class MainActivity extends AppCompatActivity {
                     return null;
                 }
                 //The temperature
-                return buffer.toString();
+
+                Gson gson = new Gson();
+                String jsonWeather = buffer.toString();
+                WeatherRec weatherRec = gson.fromJson(jsonWeather, WeatherRec.class);
+                Log.d("string：",weatherRec.list[0].weather[0].description);
+                String Newjson = gson.toJson(weatherRec);
+                return Newjson;
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -86,7 +124,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String temperature) {
             //Update the temperature displayed
-            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temperature);
+            Gson gson_layout = new Gson();
+            WeatherRec weather_layout = gson_layout.fromJson(temperature, WeatherRec.class);
+            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(Integer.toString((int)(weather_layout.list[0].main.temp - 273)));
+            ((TextView)findViewById(R.id.tv_location)).setText(weather_layout.city.name);
+            //((ImageView)findViewById(R.id.img_weather_condition)).setImageResource();
         }
     }
 }
